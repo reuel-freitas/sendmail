77692196276584f7e3a8d679ea46d212cfc2f690
Reuel Freitas Cunha <reuel.cf@outlook.com>
2021-04-05 20:41:41 -0300

    First app to send emails

---
+++ processa_envio.php
@@ -0,0 +1,10 @@
+<? 
+    require "../../app_send_mail/library/PHPMailer/Exception.php";
+    require "../../app_send_mail/library/PHPMailer/OAuth.php";
+    require "../../app_send_mail/library/PHPMailer/PHPMailer.php";
+    require "../../app_send_mail/library/PHPMailer/POP3.php";
+    require "../../app_send_mail/library/PHPMailer/SMTP.php";
+
+    require "../../app_send_mail/processa_envio.php";
+
+?>
